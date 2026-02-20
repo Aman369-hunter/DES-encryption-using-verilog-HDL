6948de442c904fee7b5acba0a441200a56daa5ad
Aman <jollyllb852965@gmail.com>
2026-02-20 19:52:50 +0530

    Add files via upload

---
+++ S-box_module.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 19.02.2026 16:00:39
+// Design Name: 
+// Module Name: S-box_module
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module S-box_module(
+    input [5:0] in,
+    output [3:0] s_out
+    );
+endmodule
